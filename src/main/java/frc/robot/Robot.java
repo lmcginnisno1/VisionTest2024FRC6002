d72be50f5daeed93cbe92cfaa76108b8df4fdc41
Lucas <lmcginnisn01@gmail.com>
2024-02-19 21:52:55 -0500

    update wpilib to 2024.3.1 and remove AdvantageKit due to incompatibility

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,17 +4,7 @@
 
 package frc.robot;
 
-import org.littletonrobotics.junction.LogFileUtil;
-import org.littletonrobotics.junction.LoggedRobot;
-import org.littletonrobotics.junction.Logger;
-import org.littletonrobotics.junction.networktables.NT4Publisher;
-import org.littletonrobotics.junction.wpilog.WPILOGReader;
-import org.littletonrobotics.junction.wpilog.WPILOGWriter;
-
-import com.pathplanner.lib.pathfinding.Pathfinding;
-
-import edu.wpi.first.wpilibj.PowerDistribution;
-import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -24,7 +14,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends LoggedRobot {
+public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
@@ -35,8 +25,6 @@ public class Robot extends LoggedRobot {
    */
   @Override
   public void robotInit(){
-    Pathfinding.setPathfinder(new LocalADStarAK());
-
     // Logger.recordMetadata("ProjectName", "FRC6002CRESCENDO-IMPORTED"); // Set a metadata value
 
     // if (isReal()) {
